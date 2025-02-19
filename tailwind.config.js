9fb0bc6b9d767558ea5bbd3a381d7073d21bd82b
Stevyussz <autogaming92@gmail.com>
2025-02-19 23:23:15 +0700

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./*.html", "./src/**/*.js"], // Sesuaikan dengan struktur proyek
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
 
 
