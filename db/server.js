7dca04dcb431ffc8fccb391a7939d8663da51f15
Qira-Qira <nawafariq3@gmail.com>
2025-03-21 00:22:23 +0700

    DONI REVISI

--- db/server.js
+++ db/server.js
@@ -21,12 +21,12 @@ app.get("/", async (req, res) => {
             jsonData = [jsonData];
         }
 
-        await collection.insertMany(jsonData);
+        // await collection.insertMany(jsonData);
 
-        const existingData = await collection.countDocuments()
-        if (existingData === 0) {
-            await collection.insertMany(jsonData);
-        }
+        // const existingData = await collection.countDocuments()
+        // if (existingData === 0) {
+        //     await collection.insertMany(jsonData);
+        // }
 
         const data = await collection.find().toArray();
 
